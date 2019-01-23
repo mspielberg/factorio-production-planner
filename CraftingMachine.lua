a8542a58d63ae3a465f84c8c5e0e30b6f7540bb6
mspielberg <9729801+mspielberg@users.noreply.github.com>
2019-01-22 16:12:50 -0800

    CraftingMachine can determine valid modules

--- CraftingMachine.lua
+++ CraftingMachine.lua
@@ -1,6 +1,126 @@
 local inspect = require "inspect"
 
+local function allowed_by_limitations(item_proto, recipe_name)
+  if item_proto.type ~= "module" then return false end
+  local limitations = item_proto.limitations
+
+  if not next(limitations) then return true end
+  for _, allowed in pairs(limitations) do
+    if recipe_name == allowed then
+      return true
+    end
+  end
+
+  return false
+end
+
+local function allowed_in_machine(self, recipe_name)
+  local out = {}
+  local allowed_effects = game.entity_prototypes[self.name].allowed_effects
+  for item_name, proto in pairs(game.item_prototypes) do
+    if allowed_by_limitations(proto, recipe_name) then
+      local all_effects_allowed = true
+      for effect_type in pairs(proto.module_effects) do
+        if not allowed_effects[effect_type] then
+          all_effects_allowed = false
+        end
+      end
+      if all_effects_allowed then
+        out[#out+1] = item_name
+      end
+    end
+  end
+  table.sort(out)
+  return out
+end
+
+local function allowed_in_beacon(allowed_by_crafting_machine, beacon_name)
+  local allowed_effects = game.entity_prototypes[beacon_name].allowed_effects
+  local allowed_by_beacon = {}
+  for _, module_name in ipairs(allowed_by_crafting_machine) do
+    local proto = game.item_prototypes[module_name]
+    local all_effects_allowed = true
+    for effect_type in pairs(proto.module_effects) do
+      if not allowed_effects[effect_type] then
+        all_effects_allowed = false
+      end
+    end
+    if all_effects_allowed then
+      allowed_by_beacon[#allowed_by_beacon+1] = module_name
+    end
+  end
+  return allowed_by_beacon
+end
+
+local function beacon_bonuses(self)
+  local beacon_info = self.beacon_info
+  if not beacon_info then
+    return 0, 0
+  end
+
+  local multiplier = beacon_info.count * beacon_info.module_count *
+    game.entity_prototypes[beacon_info.name].distribution_effectivity
+  local effects = game.item_prototypes[beacon_info.module_name].module_effects
+
+  local total_beacon_speed_bonus =
+    effects.speed and effects.speed.bonus * multiplier or 0
+  local total_beacon_productivity_bonus =
+    effects.productivity and effects.productivity.bonus * multiplier or 0
+
+  return total_beacon_speed_bonus, total_beacon_productivity_bonus
+end
+
+local function module_tooltip(self)
+  local out = {""}
+  for name, info in pairs(self.modules) do
+    out[#out+1] = {
+      "planner-gui.module-line",
+      info.count,
+      game.item_prototypes[name].localised_name,
+    }
+    out[#out+1] = "\n"
+  end
+  return out
+end
+
+local function beacon_tooltip(self)
+  local beacon_info = self.beacon_info
+  if beacon_info then
+    return {
+      "planner-gui.beacon-line",
+      beacon_info.count,
+      game.entity_prototypes[beacon_info.name].localised_name,
+      beacon_info.module_count,
+      game.item_prototypes[beacon_info.module_name].localised_name,
+    }
+  end
+  return ""
+end
+
 local CraftingMachine = {}
+
+function CraftingMachine:allowed_modules(recipe_name, beacon_name)
+  local allowed_by_crafting_machine = allowed_in_machine(self, recipe_name)
+  local allowed_in_beacon =
+    beacon_name and allowed_in_beacon(allowed_by_crafting_machine, beacon_name)
+  return {
+    allowed_by_crafting_machine = allowed_by_crafting_machine,
+    allowed_by_beacon           = allowed_in_beacon,
+  }
+end
+
+function CraftingMachine:effective_speed()
+  local total_beacon_speed_bonus, total_beacon_productivity_bonus = beacon_bonuses(self)
+  local speed_multiplier = 1 + total_beacon_speed_bonus
+  local productivity_multiplier = 1 + total_beacon_productivity_bonus
+  for _, module in pairs(self.modules) do
+    speed_multiplier = speed_multiplier +
+      (module.count * module.speed_bonus)
+    productivity_multiplier = productivity_multiplier +
+      (module.count * module.productivity_bonus)
+  end
+  return self.base_speed * speed_multiplier * productivity_multiplier
+end
 
 function CraftingMachine:set_module_count(name, count)
   if count == 0 then
@@ -29,64 +149,6 @@ function CraftingMachine:set_beacon_count(beacon_name, beacon_count, module_name
   else
     self.beacon_info = nil
   end
-end
-
-local function beacon_bonuses(self)
-  local beacon_info = self.beacon_info
-  if not beacon_info then
-    return 0, 0
-  end
-
-  local multiplier = beacon_info.count * beacon_info.module_count *
-    game.entity_prototypes[beacon_info.name].distribution_effectivity
-  local effects = game.item_prototypes[beacon_info.module_name].module_effects
-
-  local total_beacon_speed_bonus =
-    effects.speed and effects.speed.bonus * multiplier or 0
-  local total_beacon_productivity_bonus =
-    effects.productivity and effects.productivity.bonus * multiplier or 0
-
-  return total_beacon_speed_bonus, total_beacon_productivity_bonus
-end
-
-function CraftingMachine:effective_speed()
-  local total_beacon_speed_bonus, total_beacon_productivity_bonus = beacon_bonuses(self)
-  local speed_multiplier = 1 + total_beacon_speed_bonus
-  local productivity_multiplier = 1 + total_beacon_productivity_bonus
-  for _, module in pairs(self.modules) do
-    speed_multiplier = speed_multiplier +
-      (module.count * module.speed_bonus)
-    productivity_multiplier = productivity_multiplier +
-      (module.count * module.productivity_bonus)
-  end
-  return self.base_speed * speed_multiplier * productivity_multiplier
-end
-
-local function module_tooltip(self)
-  local out = {""}
-  for name, info in pairs(self.modules) do
-    out[#out+1] = {
-      "planner-gui.module-line",
-      info.count,
-      game.item_prototypes[name].localised_name,
-    }
-    out[#out+1] = "\n"
-  end
-  return out
-end
-
-local function beacon_tooltip(self)
-  local beacon_info = self.beacon_info
-  if beacon_info then
-    return {
-      "planner-gui.beacon-line",
-      beacon_info.count,
-      game.entity_prototypes[beacon_info.name].localised_name,
-      beacon_info.module_count,
-      game.item_prototypes[beacon_info.module_name].localised_name,
-    }
-  end
-  return ""
 end
 
 function CraftingMachine:tooltip()
@@ -120,23 +182,6 @@ function M.restore(self)
   return setmetatable(self, meta)
 end
 
-function M.default_crafting_machines()
-  local recipe_categories = {}
-  for _, recipe in pairs(game.recipe_prototypes) do
-    recipe_categories[recipe.category] = true
-  end
-  local crafting_machines = {}
-  for category in pairs(recipe_categories) do
-    for name, entity in pairs(game.entity_prototypes) do
-      if entity.crafting_categories and entity.crafting_categories[category] then
-        crafting_machines[category] = M.new(name)
-        break
-      end
-    end
-  end
-  return crafting_machines
-end
-
 function M.default_for(recipe)
   local proto = game.recipe_prototypes[recipe.name]
   local category = proto.category
@@ -148,15 +193,21 @@ function M.default_for(recipe)
   end
 end
 
+local entity_names_for_category_cache = {}
 function M.entity_names_for_category(category_name)
-  local out = {}
-  for entity_name, entity_proto in pairs(game.entity_prototypes) do
-    if entity_proto.crafting_categories
-    and entity_proto.crafting_categories[category] then
-      out[#out+1] = entity_name
+  local entity_names = entity_names_for_category_cache[category_name]
+  if not entity_names then
+    entity_names = {}
+    for entity_name, entity_proto in pairs(game.entity_prototypes) do
+      if entity_proto.crafting_categories
+      and entity_proto.crafting_categories[category] then
+        entity_names[#entity_names+1] = entity_name
+      end
     end
+    entity_names_for_category_cache[category_name] = entity_names
   end
-  return out
+
+  return entity_names
 end
 
 return M
--- spec/CraftingMachine_spec.lua
+++ spec/CraftingMachine_spec.lua
@@ -6,23 +6,44 @@ local function setup_mocks()
   _G.game = {
     entity_prototypes = {
       ["assembling-machine-3"] = {
+        allowed_effects = {
+          consumption = true,
+          pollution = true,
+          productivity = true,
+          speed = true,
+        },
         crafting_speed = 1.25,
       },
       ["beacon"] = {
+        allowed_effects = {
+          consumption = true,
+          pollution = true,
+          productivity = false,
+          speed = true,
+        },
         distribution_effectivity = 0.5,
       },
     },
     item_prototypes = {
+      ["iron-plate"] = {
+        limitations = nil,
+      },
       ["productivity-module-3"] = {
+        limitations = {
+          "iron-plate",
+        },
         module_effects = {
           productivity = {bonus = 0.1},
           speed = {bonus = -0.15},
         },
+        type = "module",
       },
       ["speed-module-3"] = {
+        limitations = {},
         module_effects = {
           speed = {bonus = 0.5},
         },
+        type = "module",
       },
     },
   }
@@ -33,31 +54,65 @@ describe("A CraftingMachine should", function()
     setup_mocks()
   end)
 
+  describe("know what modules it can accept", function()
+    it("with no beacon", function()
+      local uut = CraftingMachine.new("assembling-machine-3")
+      assert.are.same(
+        {
+          allowed_by_crafting_machine = {"productivity-module-3", "speed-module-3"},
+        },
+        uut:allowed_modules("iron-plate"))
+
+      assert.are.same(
+        {
+          allowed_by_crafting_machine = {"speed-module-3"},
+        },
+        uut:allowed_modules("speed-module-3"))
+    end)
+    
+    it("with a beacon", function()
+      local uut = CraftingMachine.new("assembling-machine-3")
+      assert.are.same(
+        {
+          allowed_by_crafting_machine = {"productivity-module-3", "speed-module-3"},
+          allowed_by_beacon           = {"speed-module-3"},
+        },
+        uut:allowed_modules("iron-plate", "beacon"))
+
+      assert.are.same(
+        {
+          allowed_by_crafting_machine = {"speed-module-3"},
+          allowed_by_beacon           = {"speed-module-3"},
+        },
+        uut:allowed_modules("speed-module-3", "beacon"))
+    end)
+  end)
+
   describe("calculate effective crafting rate", function()
     it("without modules", function()
       local uut = CraftingMachine.new("assembling-machine-3")
-      assert.are_equal(1.25, uut:effective_speed())
+      assert.are.equal(1.25, uut:effective_speed())
     end)
 
     it("with productivity in beacon sandwich", function()
       local uut = CraftingMachine.new("assembling-machine-3")
       uut:set_module_count("productivity-module-3", 4)
       uut:set_beacon_count("beacon", 8, "speed-module-3", 2)
-      assert.are_equal(5.5 * 1.4, uut:effective_speed())
+      assert.are.equal(5.5 * 1.4, uut:effective_speed())
     end)
 
     it("with productivity and full beacons", function()
       local uut = CraftingMachine.new("assembling-machine-3")
       uut:set_module_count("productivity-module-3", 4)
       uut:set_beacon_count("beacon", 12, "speed-module-3", 2)
-      assert.are_equal(8 * 1.4, uut:effective_speed())
+      assert.are.equal(8 * 1.4, uut:effective_speed())
     end)
 
     it("with maximum beaconed speed", function()
       local uut = CraftingMachine.new("assembling-machine-3")
       uut:set_module_count("speed-module-3", 4)
       uut:set_beacon_count("beacon", 12, "speed-module-3", 2)
-      assert.are_equal(11.25, uut:effective_speed())
+      assert.are.equal(11.25, uut:effective_speed())
     end)
   end)
 end)
